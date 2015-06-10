7578a4974142181ae12437c08089ef5ecd3371db
Sébastien Grosjean <public@zencocoon.com>
2015-06-11 00:41:30 +0300

    Add PartialModel mixin for partial model serializers

--- tests/dummy/app/serializers/user.js
+++ tests/dummy/app/serializers/user.js
@@ -1,26 +1,6 @@
 import Ember from 'ember';
 import DS from 'ember-data';
+import PartialModel from './partial-model';
+const { RESTSerializer } = DS;
 
-export default DS.RESTSerializer.extend({
-  attrs: {
-    extended: { serialize: false }
-  },
-
-  // Allow to support responses with {}
-  normalize: function(store, type, hash) {
-    var toReturn = this._super(...arguments);
-    toReturn.extended = toReturn.id;
-    return toReturn;
-  },
-
-  serialize: function(snapshot, options) {
-    // Remove partial model from here instead of attrs serialize: false
-    var ourHash = this._super(...arguments);
-    var extendedData = {};
-    var extended = snapshot.belongsTo('extended');
-    if (extended) {
-      extendedData = extended.record.serialize();
-    }
-    return Ember.merge(ourHash, extendedData);
-  }
-});
+export default RESTSerializer.extend(PartialModel, {});
