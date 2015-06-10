b1b57ca06bfc07e7b52db29d41ecc00775c3915a
Sébastien Grosjean <public@zencocoon.com>
2015-06-11 01:21:54 +0300

    Make adapter dynamically handle partial models

--- tests/dummy/app/adapters/application.js
+++ tests/dummy/app/adapters/application.js
@@ -1,11 +1,8 @@
 import Ember from 'ember';
 import DS from 'ember-data';
+import PartialModel from './partial-model';
+const { RESTAdapter } = DS;
 
-export default DS.RESTAdapter.extend({
+export default RESTAdapter.extend(PartialModel, {
   namespace: 'api'
-
-  // buildURL: function(...) {
-  //   // check if snapshot is from partial model
-  //   this._super(...)
-  // }
 });
