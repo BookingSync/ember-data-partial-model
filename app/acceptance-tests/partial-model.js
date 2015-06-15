958bfcbd35222bdb1b2261b7039eb01cb95525a2
Karol Galanciak <karol.galanciak@gmail.com>
2015-06-15 19:30:06 +0200

    Modify Store#createRecord to create partial models

---
+++ app/acceptance-tests/partial-model.js
@@ -0,0 +1,1 @@
+export { default } from 'ember-data-partial-model/acceptance-tests/partial-model';
---
+++ tests/unit/models/user-test.js
@@ -0,0 +1,18 @@
+// import {
+//   moduleForModel,
+//   test
+// } from 'ember-qunit';
+
+// import Ember from 'ember';
+
+// moduleForModel('user', {
+//   // Specify the other units that are required for this test.
+//   needs: [ { 'model:user-extended': Ember.Object.create() } ]
+// });
+
+// test('it exists', function(assert) {
+//   var model = this.subject();
+//   assert.expect(1);
+//   // var store = this.store();
+//   assert.ok(!!model);
+// });
